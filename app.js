caecd5b8f87065c874802d1c1440cdaa6feb9934
Elvin251 <emmmdov485@gmail.com>
2024-11-13 16:38:58 +0400

    d

---
+++ app.js
@@ -0,0 +1,8 @@
+let number = Number(prompt("Bir ədəd daxil edin:"));
+if (number > 0) {
+    console.log("Müsbət ədəd");
+} else if (number < 0) {
+    console.log("Mənfi ədəd");
+} else {
+    console.log("Ədəd sıfırdır");
+}
